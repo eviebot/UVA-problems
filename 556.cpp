e6149aecfa33b8e5013aadc0bda111bb68f17439
Saraansh Chopra <saraansh.chopra@gmail.com>
2017-01-19 03:15:23 +0530

    Last problem WAS TOUGH

---
+++ 556.cpp
@@ -0,0 +1,212 @@
+/*                                      .....'',;;::cccllllllllllllcccc:::;;,,,''...'',,'..
+                            ..';cldkO00KXNNNNXXXKK000OOkkkkkxxxxxddoooddddddxxxxkkkkOO0XXKx:.
+                      .':ok0KXXXNXK0kxolc:;;,,,,,,,,,,,;;,,,''''''',,''..              .'lOXKd'
+                 .,lx00Oxl:,'............''''''...................    ...,;;'.             .oKXd.
+              .ckKKkc'...'',:::;,'.........'',;;::::;,'..........'',;;;,'.. .';;'.           'kNKc.
+           .:kXXk:.    ..       ..................          .............,:c:'...;:'.         .dNNx.
+          :0NKd,          .....''',,,,''..               ',...........',,,'',,::,...,,.        .dNNx.
+         .xXd.         .:;'..         ..,'             .;,.               ...,,'';;'. ...       .oNNo
+         .0K.         .;.              ;'              ';                      .'...'.           .oXX:
+        .oNO.         .                 ,.              .     ..',::ccc:;,..     ..                lXX:
+       .dNX:               ......       ;.                'cxOKK0OXWWWWWWWNX0kc.                    :KXd.
+     .l0N0;             ;d0KKKKKXK0ko:...              .l0X0xc,...lXWWWWWWWWKO0Kx'                   ,ONKo.
+   .lKNKl...'......'. .dXWN0kkk0NWWWWWN0o.            :KN0;.  .,cokXWWNNNNWNKkxONK: .,:c:.      .';;;;:lk0XXx;
+  :KN0l';ll:'.         .,:lodxxkO00KXNWWWX000k.       oXNx;:okKX0kdl:::;'',;coxkkd, ...'. ...'''.......',:lxKO:.
+ oNNk,;c,'',.                      ...;xNNOc,.         ,d0X0xc,.     .dOd,           ..;dOKXK00000Ox:.   ..''dKO,
+'KW0,:,.,:..,oxkkkdl;'.                'KK'              ..           .dXX0o:'....,:oOXNN0d;.'. ..,lOKd.   .. ;KXl.
+;XNd,;  ;. l00kxoooxKXKx:..ld:         ;KK'                             .:dkO000000Okxl;.   c0;      :KK;   .  ;XXc
+'XXdc.  :. ..    '' 'kNNNKKKk,      .,dKNO.                                   ....       .'c0NO'      :X0.  ,.  xN0.
+.kNOc'  ,.      .00. ..''...      .l0X0d;.             'dOkxo;...                    .;okKXK0KNXx;.   .0X:  ,.  lNX'
+ ,KKdl  .c,    .dNK,            .;xXWKc.                .;:coOXO,,'.......       .,lx0XXOo;...oNWNXKk:.'KX;  '   dNX.
+  :XXkc'....  .dNWXl        .';l0NXNKl.          ,lxkkkxo' .cK0.          ..;lx0XNX0xc.     ,0Nx'.','.kXo  .,  ,KNx.
+   cXXd,,;:, .oXWNNKo'    .'..  .'.'dKk;        .cooollox;.xXXl     ..,cdOKXXX00NXc.      'oKWK'     ;k:  .l. ,0Nk.
+    cXNx.  . ,KWX0NNNXOl'.           .o0Ooldk;            .:c;.':lxOKKK0xo:,.. ;XX:   .,lOXWWXd.      . .':,.lKXd.
+     lXNo    cXWWWXooNWNXKko;'..       .lk0x;       ...,:ldk0KXNNOo:,..       ,OWNOxO0KXXNWNO,        ....'l0Xk,
+     .dNK.   oNWWNo.cXK;;oOXNNXK0kxdolllllooooddxk00KKKK0kdoc:c0No        .'ckXWWWNXkc,;kNKl.          .,kXXk,
+      'KXc  .dNWWX;.xNk.  .kNO::lodxkOXWN0OkxdlcxNKl,..        oN0'..,:ox0XNWWNNWXo.  ,ONO'           .o0Xk;
+      .ONo    oNWWN0xXWK, .oNKc       .ONx.      ;X0.          .:XNKKNNWWWWNKkl;kNk. .cKXo.           .ON0;
+      .xNd   cNWWWWWWWWKOkKNXxl:,'...;0Xo'.....'lXK;...',:lxk0KNWWWWNNKOd:..   lXKclON0:            .xNk.
+      .dXd   ;XWWWWWWWWWWWWWWWWWWNNNNNWWNNNNNNNNNWWNNNNNNWWWWWNXKNNk;..        .dNWWXd.             cXO.
+      .xXo   .ONWNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNNK0ko:'..OXo          'l0NXx,              :KK,
+      .OXc    :XNk0NWXKNWWWWWWWWWWWWWWWWWWWWWNNNX00NNx:'..       lXKc.     'lONN0l.              .oXK:
+      .KX;    .dNKoON0;lXNkcld0NXo::cd0NNO:;,,'.. .0Xc            lXXo..'l0NNKd,.              .c0Nk,
+      :XK.     .xNX0NKc.cXXl  ;KXl    .dN0.       .0No            .xNXOKNXOo,.               .l0Xk;.
+     .dXk.      .lKWN0d::OWK;  lXXc    .OX:       .ONx.     . .,cdk0XNXOd;.   .'''....;c:'..;xKXx,
+     .0No         .:dOKNNNWNKOxkXWXo:,,;ONk;,,,,,;c0NXOxxkO0XXNXKOdc,.  ..;::,...;lol;..:xKXOl.
+     ,XX:             ..';cldxkOO0KKKXXXXXXXXXXKKKKK00Okxdol:;'..   .';::,..':llc,..'lkKXkc.
+     :NX'    .     ''            ..................             .,;:;,',;ccc;'..'lkKX0d;.
+     lNK.   .;      ,lc,.         ................        ..,,;;;;;;:::,....,lkKX0d:.
+    .oN0.    .'.      .;ccc;,'....              ....'',;;;;;;;;;;'..   .;oOXX0d:.
+    .dN0.      .;;,..       ....                ..''''''''....     .:dOKKko;.
+     lNK'         ..,;::;;,'.........................           .;d0X0kc'.
+     .xXO'                                                 .;oOK0x:.
+      .cKKo.                                    .,:oxkkkxk0K0xc'.
+        .oKKkc,.                         .';cok0XNNNX0Oxoc,.
+          .;d0XX0kdlc:;,,,',,,;;:clodkO0KK0Okdl:,'..
+              .,coxO0KXXXXXXXKK0OOxdoc:,..
+  ░░░░░░░░░░░░▄▄▄▄░░░░░░░░░░░░░░░░░░░░░░░▄▄▄▄▄░░░░
+  ░░░█░░░░▄▀█▀▀▄░░▀▀▀▄░░░░▐█░░░░░░░░░▄▀█▀▀▄░░░▀█▄░
+  ░░█░░░░▀░▐▌░░▐▌░░░░░▀░░░▐█░░░░░░░░▀░▐▌░░▐▌░░░░█▀
+  ░▐▌░░░░░░░▀▄▄▀░░░░░░░░░░▐█▄▄░░░░░░░░░▀▄▄▀░░░░░▐▌
+  ░█░░░░░░░░░░░░░░░░░░░░░░░░░▀█░░░░░░░░░░░░░░░░░░█
+  ▐█░░░░░░░░░░░░░░░░░░░░░░░░░░█▌░░░░░░░░░░░░░░░░░█   - PROPRIETARY CODE OF EVIEBOT
+  ▐█░░░░░░░░░░░░░░░░░░░░░░░░░░█▌░░░░░░░░░░░░░░░░░█
+  ░█░░░░░░░░░░░░░░░░░░░░█▄░░░▄█░░░░░░░░░░░░░░░░░░█
+  ░▐▌░░░░░░░░░░░░░░░░░░░░▀███▀░░░░░░░░░░░░░░░░░░▐▌
+  ░░█░░░░░░░░░░░░░░░░░▀▄░░░░░░░░░░▄▀░░░░░░░░░░░░█░
+  ░░░█░░░░░░░░░░░░░░░░░░▀▄▄▄▄▄▄▄▀▀░░░░░░░░░░░░░█░
+  ▒▒▒░░░░░░░░░░▄▐░░░░
+  ▒░░░░░░▄▄▄░░▄██▄░░░
+  ░░░░░░▐▀█▀▌░░░░▀█▄░
+  ░░░░░░▐█▄█▌░░░░░░▀█▄
+  ░░░░░░░▀▄▀░░░▄▄▄▄▄▀▀
+  ░░░░░▄▄▄██▀▀▀▀░░░░░
+  ░░░░█▀▄▄▄█░▀▀░░░░░░
+  ░░░░▌░▄▄▄▐▌▀▀▀░░░░░
+  ░▄░▐░░░▄▄░█░▀▀░░░░░   DON'T MESS WITH MR. SKELTAL
+  ░▀█▌░░░▄░▀█▀░▀░░░░░
+  ░░░░░░░░▄▄▐▌▄▄░░░░░
+  ░░░░░░░░▀███▀█░▄░░░
+  ░░░░░░░▐▌▀▄▀▄▀▐▄░░░
+  ░░░░░░░▐▀░░░░░░▐▌░░
+  ░░░░░░░█░░░░░░░░█░░
+  ░░░░░░▐▌░░░░░░░░░█░  */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define pb push_back
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int b, w, i, j, counter[5];
+    bool exit_flag;
+    ofstream outfile;
+    outfile.open("output.txt");
+    char c;
+    while(true)
+    {
+        cin >> b >> w;
+        if(b==0 && w==0)
+            break;
+        for(i=0; i< 5;i++)
+            counter[i] =0;
+        int arr[b+2][w+2], pos[2] = {b, 1}, current_dir[2] = {0, 1};exit_flag=false;
+        for(i = 0; i<b+2;i++)
+        {
+            for(j=0;j<w+2;j++)
+                arr[i][j] = 0;
+        }
+
+        for(i= 1; i< b+1;i++)
+        {
+            for(j = 1; j< w+1; j++)
+            {
+                cin >> c;
+                arr[i][j] = !((int)(c)-48);
+            }
+        }
+        while(true)
+        {
+            /*for(i = 0; i< b+2;i++)
+            {
+                for(j= 0; j< w+2;j++)
+                    cout << arr[i][j];
+                cout << "\n";
+            }
+            cout << current_dir[0] << current_dir[1] <<"\n";getchar();cout.flush();*/
+            pos[0]  += current_dir[0];
+            pos[1]  += current_dir[1];
+            if(pos[0] == b && pos[1] ==1&& exit_flag)
+                break;
+            if(arr[pos[0]][pos[1]] == 0)
+            {
+                pos[0] -= current_dir[0];
+                pos[1] -= current_dir[1];
+
+                if(current_dir[0] == 0 && current_dir[1] ==1)
+                {
+                    current_dir[0] = -1;
+                    current_dir[1] = 0;
+                }
+                else if(current_dir[0] == -1 && current_dir[1] ==0)
+                {
+                    current_dir[0] = 0;
+                    current_dir[1] = -1;
+                }
+                else if(current_dir[0] == 0 && current_dir[1] == -1)
+                {
+                    current_dir[0] = 1;
+                    current_dir[1] = 0;
+                }
+                else
+                {
+                    current_dir[0] = 0;
+                    current_dir[1] = 1;
+                }
+                continue;
+            }
+            else if(current_dir[1]==1 && arr[pos[0]+1][pos[1]]!=0)
+            {
+                arr[pos[0]][pos[1]] ++;
+                pos[0] ++;
+                arr[pos[0]][pos[1]] ++;
+                current_dir[0]=1;current_dir[1] = 0;
+            }
+            else if(current_dir[0] == -1 && arr[pos[0]][pos[1]+1]!=0)
+            {
+                arr[pos[0]][pos[1]] ++;
+                pos[1]++;
+                arr[pos[0]][pos[1]] ++;
+                current_dir[0]=0;current_dir[1] = 1;
+            }
+            else if(current_dir[1] == -1 && arr[pos[0]-1][pos[1]]!=0)
+            {
+                arr[pos[0]][pos[1]] ++;
+                pos[0]--;
+                arr[pos[0]][pos[1]] ++;
+                current_dir[0]=-1;current_dir[1]=0;
+            }
+            else if(current_dir[0]==1 && arr[pos[0]][pos[1]-1]!=0)
+            {
+                arr[pos[0]][pos[1]] ++;
+                pos[1]--;
+                arr[pos[0]][pos[1]] ++;
+                current_dir[0] = 0;
+                current_dir[1] = -1;
+            }
+            else
+            {
+                arr[pos[0]][pos[1]] ++;
+            }
+            exit_flag=true;
+            if(pos[0] == b && pos[1] ==1&& exit_flag)
+                break;
+        }
+
+        arr[b][1] =2;
+
+        for(i = 1; i< b+1; i++)
+        {
+            for(j = 1; j<w+1;j++)
+            {
+                if(arr[i][j]!=0)
+                {
+                    counter[arr[i][j]-1]++;
+                }
+
+            }
+        }
+
+        for(i=0; i< 5;i++)
+        {
+            cout << (counter[i]>=10?" ":"  ")<<counter[i];
+        }
+        cout << "\n";
+    }
+}
+
