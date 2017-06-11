00a3e12f3262426b755d65169efaeffb3d514d22
Saraansh Chopra <saraansh.chopra@gmail.com>
2017-06-11 16:15:14 +0530

    1-31 June 2017

---
+++ 10337.cpp
@@ -0,0 +1,145 @@
+/*                                      .....'',;;::cccllllllllllllcccc:::;;,,,''...'',,'..
+                            ..';cldkO00KXNNNNXXXKK000OOkkkkkxxxxxddoooddddddxxxxkkkkOO0XXKx:.
+                      .':ok0KXXXNXK0kxolc:;;,,,,,,,,,,,;;,,,''''''',,''..              .'lOXKd'
+                 .,lx00Oxl:,'............''''''...................    ...,;;'.             .oKXd.
+              .ckKKkc'...'',:::;,'.........'',;;::::;,'..........'',;;;,'.. .';;'.           'kNKc.
+           .:kXXk:.    ..       ..................          .............,:c:'...;:'.         .dNNx.
+          :0NKd,          .....''',,,,''..               ',...........',,,'',,::,...,,.        .dNNx.
+         .xXd.         .:;'..         ..,'             .;,.               ...,,'';;'. ...       .oNNo
+         .0K.         .;.              ;'              ';                      .'...'.           .oXX:
+        .oNO.         .                 ,.              .     ..',::ccc:;,..     ..                lXX:
+       .dNX:               ......       ;.                'cxOKK0OXWWWWWWWNX0kc.                    :KXd.
+     .l0N0;             ;d0KKKKKXK0ko:...              .l0X0xc,...lXWWWWWWWWKO0Kx'                   ,ONKo.
+   .lKNKl...'......'. .dXWN0kkk0NWWWWWN0o.            :KN0;.  .,cokXWWNNNNWNKkxONK: .,:c:.      .';;;;:lk0XXx;
+  :KN0l';ll:'.         .,:lodxxkO00KXNWWWX000k.       oXNx;:okKX0kdl:::;'',;coxkkd, ...'. ...'''.......',:lxKO:.
+ oNNk,;c,'',.                      ...;xNNOc,.         ,d0X0xc,.     .dOd,           ..;dOKXK00000Ox:.   ..''dKO,
+'KW0,:,.,:..,oxkkkdl;'.                'KK'              ..           .dXX0o:'....,:oOXNN0d;.'. ..,lOKd.   .. ;KXl.
+;XNd,;  ;. l00kxoooxKXKx:..ld:         ;KK'                             .:dkO000000Okxl;.   c0;      :KK;   .  ;XXc
+'XXdc.  :. ..    '' 'kNNNKKKk,      .,dKNO.                                   ....       .'c0NO'      :X0.  ,.  xN0.
+.kNOc'  ,.      .00. ..''...      .l0X0d;.             'dOkxo;...                    .;okKXK0KNXx;.   .0X:  ,.  lNX'
+ ,KKdl  .c,    .dNK,            .;xXWKc.                .;:coOXO,,'.......       .,lx0XXOo;...oNWNXKk:.'KX;  '   dNX.
+  :XXkc'....  .dNWXl        .';l0NXNKl.          ,lxkkkxo' .cK0.          ..;lx0XNX0xc.     ,0Nx'.','.kXo  .,  ,KNx.
+   cXXd,,;:, .oXWNNKo'    .'..  .'.'dKk;        .cooollox;.xXXl     ..,cdOKXXX00NXc.      'oKWK'     ;k:  .l. ,0Nk.
+    cXNx.  . ,KWX0NNNXOl'.           .o0Ooldk;            .:c;.':lxOKKK0xo:,.. ;XX:   .,lOXWWXd.      . .':,.lKXd.
+     lXNo    cXWWWXooNWNXKko;'..       .lk0x;       ...,:ldk0KXNNOo:,..       ,OWNOxO0KXXNWNO,        ....'l0Xk,
+     .dNK.   oNWWNo.cXK;;oOXNNXK0kxdolllllooooddxk00KKKK0kdoc:c0No        .'ckXWWWNXkc,;kNKl.          .,kXXk,
+      'KXc  .dNWWX;.xNk.  .kNO::lodxkOXWN0OkxdlcxNKl,..        oN0'..,:ox0XNWWNNWXo.  ,ONO'           .o0Xk;
+      .ONo    oNWWN0xXWK, .oNKc       .ONx.      ;X0.          .:XNKKNNWWWWNKkl;kNk. .cKXo.           .ON0;
+      .xNd   cNWWWWWWWWKOkKNXxl:,'...;0Xo'.....'lXK;...',:lxk0KNWWWWNNKOd:..   lXKclON0:            .xNk.
+      .dXd   ;XWWWWWWWWWWWWWWWWWWNNNNNWWNNNNNNNNNWWNNNNNNWWWWWNXKNNk;..        .dNWWXd.             cXO.
+      .xXo   .ONWNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNNK0ko:'..OXo          'l0NXx,              :KK,
+      .OXc    :XNk0NWXKNWWWWWWWWWWWWWWWWWWWWWNNNX00NNx:'..       lXKc.     'lONN0l.              .oXK:
+      .KX;    .dNKoON0;lXNkcld0NXo::cd0NNO:;,,'.. .0Xc            lXXo..'l0NNKd,.              .c0Nk,
+      :XK.     .xNX0NKc.cXXl  ;KXl    .dN0.       .0No            .xNXOKNXOo,.               .l0Xk;.
+     .dXk.      .lKWN0d::OWK;  lXXc    .OX:       .ONx.     . .,cdk0XNXOd;.   .'''....;c:'..;xKXx,
+     .0No         .:dOKNNNWNKOxkXWXo:,,;ONk;,,,,,;c0NXOxxkO0XXNXKOdc,.  ..;::,...;lol;..:xKXOl.
+     ,XX:             ..';cldxkOO0KKKXXXXXXXXXXKKKKK00Okxdol:;'..   .';::,..':llc,..'lkKXkc.
+     :NX'    .     ''            ..................             .,;:;,',;ccc;'..'lkKX0d;.
+     lNK.   .;      ,lc,.         ................        ..,,;;;;;;:::,....,lkKX0d:.
+    .oN0.    .'.      .;ccc;,'....              ....'',;;;;;;;;;;'..   .;oOXX0d:.
+    .dN0.      .;;,..       ....                ..''''''''....     .:dOKKko;.
+     lNK'         ..,;::;;,'.........................           .;d0X0kc'.
+     .xXO'                                                 .;oOK0x:.
+      .cKKo.                                    .,:oxkkkxk0K0xc'.
+        .oKKkc,.                         .';cok0XNNNX0Oxoc,.
+          .;d0XX0kdlc:;,,,',,,;;:clodkO0KK0Okdl:,'..
+              .,coxO0KXXXXXXXKK0OOxdoc:,..
+  ░░░░░░░░░░░░▄▄▄▄░░░░░░░░░░░░░░░░░░░░░░░▄▄▄▄▄░░░░
+  ░░░█░░░░▄▀█▀▀▄░░▀▀▀▄░░░░▐█░░░░░░░░░▄▀█▀▀▄░░░▀█▄░
+  ░░█░░░░▀░▐▌░░▐▌░░░░░▀░░░▐█░░░░░░░░▀░▐▌░░▐▌░░░░█▀
+  ░▐▌░░░░░░░▀▄▄▀░░░░░░░░░░▐█▄▄░░░░░░░░░▀▄▄▀░░░░░▐▌
+  ░█░░░░░░░░░░░░░░░░░░░░░░░░░▀█░░░░░░░░░░░░░░░░░░█
+  ▐█░░░░░░░░░░░░░░░░░░░░░░░░░░█▌░░░░░░░░░░░░░░░░░█   - PROPRIETARY CODE OF EVIEBOT
+  ▐█░░░░░░░░░░░░░░░░░░░░░░░░░░█▌░░░░░░░░░░░░░░░░░█
+  ░█░░░░░░░░░░░░░░░░░░░░█▄░░░▄█░░░░░░░░░░░░░░░░░░█
+  ░▐▌░░░░░░░░░░░░░░░░░░░░▀███▀░░░░░░░░░░░░░░░░░░▐▌
+  ░░█░░░░░░░░░░░░░░░░░▀▄░░░░░░░░░░▄▀░░░░░░░░░░░░█░
+  ░░░█░░░░░░░░░░░░░░░░░░▀▄▄▄▄▄▄▄▀▀░░░░░░░░░░░░░█░
+  ▒▒▒░░░░░░░░░░▄▐░░░░
+  ▒░░░░░░▄▄▄░░▄██▄░░░
+  ░░░░░░▐▀█▀▌░░░░▀█▄░
+  ░░░░░░▐█▄█▌░░░░░░▀█▄
+  ░░░░░░░▀▄▀░░░▄▄▄▄▄▀▀
+  ░░░░░▄▄▄██▀▀▀▀░░░░░
+  ░░░░█▀▄▄▄█░▀▀░░░░░░
+  ░░░░▌░▄▄▄▐▌▀▀▀░░░░░
+  ░▄░▐░░░▄▄░█░▀▀░░░░░   DON'T MESS WITH MR. SKELTAL
+  ░▀█▌░░░▄░▀█▀░▀░░░░░
+  ░░░░░░░░▄▄▐▌▄▄░░░░░
+  ░░░░░░░░▀███▀█░▄░░░
+  ░░░░░░░▐▌▀▄▀▄▀▐▄░░░
+  ░░░░░░░▐▀░░░░░░▐▌░░
+  ░░░░░░░█░░░░░░░░█░░
+  ░░░░░░▐▌░░░░░░░░░█░  */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define MP make_pair
+
+typedef long long int lli;
+ofstream outfile;
+ifstream infile;
+
+int main()
+{
+    infile.open("input.txt");
+    outfile.open("output.txt");
+
+    ios::sync_with_stdio(false);cout.tie(0);cin.tie(0);
+
+    int T, n, i, j, t;
+    cin >> T;
+
+    for(int z = 0; z < T; ++z)
+    {
+        deque <vector <int>> storage;
+        cin >> n;
+        n/= 100;
+        vector <int> temp(n);
+        for(j = 0; j< 10; ++j)
+        {
+            for(i = 0; i< n; ++i)
+            {
+                cin >> t;
+                temp[i] = t;
+            }
+            storage.pf(temp);
+        }
+
+        int DP[10][n+1];
+        for(i = 0; i< 10 ; ++i)
+        {
+            for(j = 0; j<= n; ++j)
+                DP[i][j] = 1e5;
+        }
+        DP[0][0] = 0;
+        for(i = 1; i<= n; ++i)
+        {
+            for(j = 0; j<= min(i, 9); ++j)
+            {
+                if( j == 0)
+                {
+                    //if( i == 1)
+                        //cout << DP[j][i-1] + 30 - storage[j][i-1]<<"\n";
+                    DP[j][i] = min(DP[j][i-1] + 30 - storage[j][i-1], DP[j+1][i-1] + 20 - storage[j+1][i-1]);
+                }
+                else if(j == 9)
+                {
+                    DP[j][i] = min(DP[j][i-1] + 30 - storage[j][i-1], DP[j-1][i-1] + 60 - storage[j-1][i-1]);
+                }
+                else
+                {
+                    DP[j][i] = min(DP[j][i-1] + 30 - storage[j][i-1], min(DP[j-1][i-1] + 60 - storage[j-1][i-1], DP[j+1][i-1] + 20 - storage[j+1][i-1]));
+                }
+            }
+        }
+        cout << DP[0][n] << "\n\n";
+    }
+}
