6b5e9687261cff063b36e82480dc5ed2f165c933
Saraansh Chopra <saraansh.chopra@gmail.com>
2017-06-16 13:24:48 +0530

    1-31 June 2017

---
+++ 11060.cpp
@@ -0,0 +1,192 @@
+/*                                      .....'',;;::cccllllllllllllcccc:::;;,,,''...'',,'..
+                            ..';cldkO00KXNNNNXXXKK000OOkkkkkxxxxxddoooddddddxxxxkkkkOO0XXKx:.
+                      .':ok0KXXXNXK0kxolc:;;,,,,,,,,,,,;;,,,''''''',,''..              .'lOXKd'
+                 .,lx00Oxl:,'............''''''...................    ...,;;'.             .oKXd.
+              .ckKKkc'...'',:::;,'.........'',;;::::;,'..........'',;;;,'.. .';;'.           'kNKc.
+           .:kXXk:.    ..       ..................          .............,:c:'...;:'.         .dNNx.
+          :0NKd,          .....''',,,,''..               ',...........',,,'',,::,...,,.        .dNNx.
+         .xXd.         .:;'..         ..,'             .;,.               ...,,'';;'. ...       .oNNo
+         .0K.         .;.              ;'              ';                      .'...'.           .oXX:
+        .oNO.         .                 ,.              .     ..',::ccc:;,..     ..                lXX:
+       .dNX:               ......       ;.                'cxOKK0OXWWWWWWWNX0kc.                    :KXd.
+     .l0N0;             ;d0KKKKKXK0ko:...              .l0X0xc,...lXWWWWWWWWKO0Kx'                   ,ONKo.
+   .lKNKl...'......'. .dXWN0kkk0NWWWWWN0o.            :KN0;.  .,cokXWWNNNNWNKkxONK: .,:c:.      .';;;;:lk0XXx;
+  :KN0l';ll:'.         .,:lodxxkO00KXNWWWX000k.       oXNx;:okKX0kdl:::;'',;coxkkd, ...'. ...'''.......',:lxKO:.
+ oNNk,;c,'',.                      ...;xNNOc,.         ,d0X0xc,.     .dOd,           ..;dOKXK00000Ox:.   ..''dKO,
+'KW0,:,.,:..,oxkkkdl;'.                'KK'              ..           .dXX0o:'....,:oOXNN0d;.'. ..,lOKd.   .. ;KXl.
+;XNd,;  ;. l00kxoooxKXKx:..ld:         ;KK'                             .:dkO000000Okxl;.   c0;      :KK;   .  ;XXc
+'XXdc.  :. ..    '' 'kNNNKKKk,      .,dKNO.                                   ....       .'c0NO'      :X0.  ,.  xN0.
+.kNOc'  ,.      .00. ..''...      .l0X0d;.             'dOkxo;...                    .;okKXK0KNXx;.   .0X:  ,.  lNX'
+ ,KKdl  .c,    .dNK,            .;xXWKc.                .;:coOXO,,'.......       .,lx0XXOo;...oNWNXKk:.'KX;  '   dNX.
+  :XXkc'....  .dNWXl        .';l0NXNKl.          ,lxkkkxo' .cK0.          ..;lx0XNX0xc.     ,0Nx'.','.kXo  .,  ,KNx.
+   cXXd,,;:, .oXWNNKo'    .'..  .'.'dKk;        .cooollox;.xXXl     ..,cdOKXXX00NXc.      'oKWK'     ;k:  .l. ,0Nk.
+    cXNx.  . ,KWX0NNNXOl'.           .o0Ooldk;            .:c;.':lxOKKK0xo:,.. ;XX:   .,lOXWWXd.      . .':,.lKXd.
+     lXNo    cXWWWXooNWNXKko;'..       .lk0x;       ...,:ldk0KXNNOo:,..       ,OWNOxO0KXXNWNO,        ....'l0Xk,
+     .dNK.   oNWWNo.cXK;;oOXNNXK0kxdolllllooooddxk00KKKK0kdoc:c0No        .'ckXWWWNXkc,;kNKl.          .,kXXk,
+      'KXc  .dNWWX;.xNk.  .kNO::lodxkOXWN0OkxdlcxNKl,..        oN0'..,:ox0XNWWNNWXo.  ,ONO'           .o0Xk;
+      .ONo    oNWWN0xXWK, .oNKc       .ONx.      ;X0.          .:XNKKNNWWWWNKkl;kNk. .cKXo.           .ON0;
+      .xNd   cNWWWWWWWWKOkKNXxl:,'...;0Xo'.....'lXK;...',:lxk0KNWWWWNNKOd:..   lXKclON0:            .xNk.
+      .dXd   ;XWWWWWWWWWWWWWWWWWWNNNNNWWNNNNNNNNNWWNNNNNNWWWWWNXKNNk;..        .dNWWXd.             cXO.
+      .xXo   .ONWNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNNK0ko:'..OXo          'l0NXx,              :KK,
+      .OXc    :XNk0NWXKNWWWWWWWWWWWWWWWWWWWWWNNNX00NNx:'..       lXKc.     'lONN0l.              .oXK:
+      .KX;    .dNKoON0;lXNkcld0NXo::cd0NNO:;,,'.. .0Xc            lXXo..'l0NNKd,.              .c0Nk,
+      :XK.     .xNX0NKc.cXXl  ;KXl    .dN0.       .0No            .xNXOKNXOo,.               .l0Xk;.
+     .dXk.      .lKWN0d::OWK;  lXXc    .OX:       .ONx.     . .,cdk0XNXOd;.   .'''....;c:'..;xKXx,
+     .0No         .:dOKNNNWNKOxkXWXo:,,;ONk;,,,,,;c0NXOxxkO0XXNXKOdc,.  ..;::,...;lol;..:xKXOl.
+     ,XX:             ..';cldxkOO0KKKXXXXXXXXXXKKKKK00Okxdol:;'..   .';::,..':llc,..'lkKXkc.
+     :NX'    .     ''            ..................             .,;:;,',;ccc;'..'lkKX0d;.
+     lNK.   .;      ,lc,.         ................        ..,,;;;;;;:::,....,lkKX0d:.
+    .oN0.    .'.      .;ccc;,'....              ....'',;;;;;;;;;;'..   .;oOXX0d:.
+    .dN0.      .;;,..       ....                ..''''''''....     .:dOKKko;.
+     lNK'         ..,;::;;,'.........................           .;d0X0kc'.
+     .xXO'                                                 .;oOK0x:.
+      .cKKo.                                    .,:oxkkkxk0K0xc'.
+        .oKKkc,.                         .';cok0XNNNX0Oxoc,.
+          .;d0XX0kdlc:;,,,',,,;;:clodkO0KK0Okdl:,'..
+              .,coxO0KXXXXXXXKK0OOxdoc:,..
+  ░░░░░░░░░░░░▄▄▄▄░░░░░░░░░░░░░░░░░░░░░░░▄▄▄▄▄░░░░
+  ░░░█░░░░▄▀█▀▀▄░░▀▀▀▄░░░░▐█░░░░░░░░░▄▀█▀▀▄░░░▀█▄░
+  ░░█░░░░▀░▐▌░░▐▌░░░░░▀░░░▐█░░░░░░░░▀░▐▌░░▐▌░░░░█▀
+  ░▐▌░░░░░░░▀▄▄▀░░░░░░░░░░▐█▄▄░░░░░░░░░▀▄▄▀░░░░░▐▌
+  ░█░░░░░░░░░░░░░░░░░░░░░░░░░▀█░░░░░░░░░░░░░░░░░░█
+  ▐█░░░░░░░░░░░░░░░░░░░░░░░░░░█▌░░░░░░░░░░░░░░░░░█   - PROPRIETARY CODE OF EVIEBOT
+  ▐█░░░░░░░░░░░░░░░░░░░░░░░░░░█▌░░░░░░░░░░░░░░░░░█
+  ░█░░░░░░░░░░░░░░░░░░░░█▄░░░▄█░░░░░░░░░░░░░░░░░░█
+  ░▐▌░░░░░░░░░░░░░░░░░░░░▀███▀░░░░░░░░░░░░░░░░░░▐▌
+  ░░█░░░░░░░░░░░░░░░░░▀▄░░░░░░░░░░▄▀░░░░░░░░░░░░█░
+  ░░░█░░░░░░░░░░░░░░░░░░▀▄▄▄▄▄▄▄▀▀░░░░░░░░░░░░░█░
+  ▒▒▒░░░░░░░░░░▄▐░░░░
+  ▒░░░░░░▄▄▄░░▄██▄░░░
+  ░░░░░░▐▀█▀▌░░░░▀█▄░
+  ░░░░░░▐█▄█▌░░░░░░▀█▄
+  ░░░░░░░▀▄▀░░░▄▄▄▄▄▀▀
+  ░░░░░▄▄▄██▀▀▀▀░░░░░
+  ░░░░█▀▄▄▄█░▀▀░░░░░░
+  ░░░░▌░▄▄▄▐▌▀▀▀░░░░░
+  ░▄░▐░░░▄▄░█░▀▀░░░░░   DON'T MESS WITH MR. SKELTAL
+  ░▀█▌░░░▄░▀█▀░▀░░░░░
+  ░░░░░░░░▄▄▐▌▄▄░░░░░
+  ░░░░░░░░▀███▀█░▄░░░
+  ░░░░░░░▐▌▀▄▀▄▀▐▄░░░
+  ░░░░░░░▐▀░░░░░░▐▌░░
+  ░░░░░░░█░░░░░░░░█░░
+  ░░░░░░▐▌░░░░░░░░░█░  */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define MP make_pair
+
+typedef long long int lli;
+ofstream outfile;
+ifstream infile;
+
+bool visited[101];
+int t, topo[101];
+vector <vector <int>> graph, rev_graph;
+vector <int> temp;
+priority_queue <pair <int, int>, vector <pair <int, int>>, greater <pair <int, int>> > myq;
+
+void DFS_2(int root, int level)
+{
+    if(visited[root])
+        return;
+    visited[root] = true;
+    myq.push(MP(level, root));
+    for(int i = 0 ; i < graph[root].size(); ++i)
+    {
+        DFS_2(graph[root][i], level+1);
+    }
+    topo[t--] = root;
+}
+
+int superroot(int child)
+{
+    if(rev_graph[child].size())
+    {
+        return superroot(rev_graph[child][0]);
+    }
+    else
+        return child;
+}
+bool comp(int a, int b)
+{
+    return a > b;
+}
+int main()
+{
+    infile.open("input.txt");
+    outfile.open("output.txt");
+
+    //ios::sync_with_stdio(false);cout.tie(0);cin.tie(0);
+
+    int counter = 1, n, m, i;
+    string s, s2;
+    while(infile >> n)
+    {
+        t = n-1;
+        myq = priority_queue <pair <int, int>, vector <pair <int,int>> , greater <pair <int, int>> >();
+        vector <string> myvec;
+        myvec.pb("");
+        unordered_map <string, int> mymap;
+        unordered_map <string, int>::iterator it;
+        graph.clear();
+        rev_graph.clear();
+        graph.pb(temp);
+        rev_graph.pb(temp);
+        for(i = 1; i <= n; ++i)
+        {
+            rev_graph.pb(temp);
+            graph.pb(temp);
+            visited[i] = false;
+
+            infile>> s;
+
+            mymap.insert( MP(s, i) );
+            myvec.pb(s);
+        }
+        infile >> m;
+
+        for(i = 0; i < m; ++i)
+        {
+            infile >> s >> s2;
+
+            graph[mymap.find(s)->se].pb( mymap.find(s2)->se );
+            rev_graph[mymap.find(s2)->se].pb(mymap.find(s)->se);
+        }
+
+        /*for(i = 1; i <= n; ++i)
+        {
+            sort(graph[i].begin(), graph[i].end(), comp);
+        }*/
+
+        /*for(i = 1; i <= n; ++i)
+        {
+            cout << myvec[i] << " = ";
+            for(int j = 0; j < graph[i].size(); ++j)
+            {
+                cout << myvec[graph[i][j]] << ' ';
+            }
+            cout << '\n';
+        }*/
+        for(i = 1; i <= n; ++i)
+        {
+            if( !visited[i]){cout << "super of "<< i << " is "<< superroot(i)<< '\n';
+                DFS_2(superroot(i), 1);}
+        }
+
+        cout << "Case #"<< counter++<<": Dilbert should drink beverages in this order:";
+        for(i = 0; i < n; ++i)
+        {
+            cout << " "<<myvec[myq.top().se];
+            myq.pop();
+        }
+        cout << ".\n\n";
+    }
+}
